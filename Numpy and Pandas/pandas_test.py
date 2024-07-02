20262c50620b5db34d2032399cad80d3d61425a7
prakhar.newatia <prakhar.newatia@accrete.ai>
2024-07-02 09:02:37 +0530

    pandas course added

--- Numpy and Pandas/pandas_test.py
+++ Numpy and Pandas/pandas_test.py
@@ -39,3 +39,29 @@ df4 = df2[~condition]
 df4["test"] = df4["Profit"] / df4["Quantity"]
 
 df4 = df4[df4["Category"] == "Technology"].sort_values(by="Sales", ascending=True)
+
+
+
+import numpy as np
+import pandas as pd
+
+# The file is stored at the following path:
+# 'https://media-doselect.s3.amazonaws.com/generic/NMgEjwkAEGGQZBoNYGr9Ld7w0/rating.csv'
+df = pd.read_csv('https://media-doselect.s3.amazonaws.com/generic/NMgEjwkAEGGQZBoNYGr9Ld7w0/rating.csv')
+
+df["Training"] = df.Rating.apply(lambda x: "Yes" if x<3.5 else "No")
+# Provide your answer below
+
+print(df.head(5))
+
+#  In the dataframe created above, find the department that has the most efficient team (the team with minimum percentage of employees who need training).
+dep = "Sales"
+n1 = df[(df["Training"]=="Yes") & (df["Department"]==dep)]["Department"].count()
+n2 = df[df["Department"]==dep].Department.count()
+
+print(n1/n2)
+
+rating = df
+
+for i in ['Finance', 'HR', 'Sales', 'Marketing']:
+    print(i, len(rating[(rating['Training'] == 'No') & (rating['Department'] == i)]) / len(rating[rating['Department'] == i]) * 100)
